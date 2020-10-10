0c48cf80c3c2d134a74068afc0b66751aeadcfa0
DavidLee1995 <17620711892@163.com>
2020-10-10 10:13:02 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<!--
+ * @Description:
+ * @Version: 2.0
+ * @Autor: LXL
+ * @Date: 2020-10-07 22:28:31
+ * @LastEditors: LXL
+ * @LastEditTime: 2020-10-08 00:28:21
+-->
+<template>
+  <div class="container">
+    <ColumnList :list="list"></ColumnList>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1专栏，有一段简单的介绍',
+    avatar: require('./assets/logo.png')
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是test2专栏，有一段简单的介绍'
+  }, {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是test3专栏，有一段简单的介绍',
+    avatar: require('./assets/logo.png')
+  }, {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是test4专栏，有一段简单的介绍',
+    avatar: require('./assets/logo.png')
+  }
+]
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Description:
+ * @Version: 2.0
+ * @Autor: LXL
+ * @Date: 2020-10-08 13:28:29
+ * @LastEditors: LXL
+ * @LastEditTime: 2020-10-08 13:29:55
+-->
+<template>
+    <h1></h1>
+</template>
+<script lang='ts'>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'GlobalHeader'
+})
+</script>
