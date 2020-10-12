06bdd262d8d48baac2f1d6e932d0bf28a65f052c
DavidLee1995 <17620711892@163.com>
2020-10-12 08:31:54 +0800

    测试

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,58 @@
+<!--
+ * @Description:
+ * @Version: 2.0
+ * @Autor: LXL
+ * @Date: 2020-10-10 13:24:06
+ * @LastEditors: LXL
+ * @LastEditTime: 2020-10-10 16:56:08
+-->
+<template>
+  <div class="dropdown" ref="dropdownRef">
+    <a
+      href="#"
+      class="btn btn-outline-light my-2 dropdown-toggle"
+      @click.prevent="toggleOpen"
+      >{{ title }}</a
+    >
+    <ul class="dropdown-menu" :style="{ display: 'block' }" v-if="isOpen">
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, onUnmounted, ref } from 'vue'
+export default defineComponent({
+  name: 'Dropdown',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup () {
+    const isOpen = ref(false)
+    const dropdownRef = ref < null | HTMLElement >(null)
+    const toggleOpen = () => {
+      isOpen.value = !isOpen.value
+    }
+    const handler = (e: MouseEvent) => {
+      if (dropdownRef.value) {
+        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+          isOpen.value = false
+        }
+      }
+    }
+    onMounted(() => {
+      document.addEventListener('click', handler)
+    })
+    onUnmounted(() => {
+      document.removeEventListener('click', handler)
+    })
+    return {
+      isOpen,
+      toggleOpen,
+      dropdownRef
+    }
+  }
+})
+</script>
---
+++ src/components/DropdownItem.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Description:
+ * @Version: 2.0
+ * @Autor: LXL
+ * @Date: 2020-10-10 14:38:54
+ * @LastEditors: LXL
+ * @LastEditTime: 2020-10-10 16:46:44
+-->
+<template>
+  <li class="dropdown-oprion" :class="{ 'is-disabled': disabled }">
+    <slot></slot>
+  </li>
+</template>
+<script lang='ts'>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'DropdownItem',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+</script>
+<style >
+.dropdown-oprion.is-disabled * {
+  color: #6c757d;
+  pointer-events: none;
+  background-color: transparent;
+}
+</style>
